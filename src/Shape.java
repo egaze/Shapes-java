fb12acb3167e7d4d4f19199df66c379e41827d26
Eyiram Gaze <eyiramgaze@gmail.com>
2024-10-31 22:39:08 +0000

    Implementing shape parent class

---
+++ src/Shape.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Shape {
+    private Point center;
+    private Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
